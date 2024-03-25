a16f33f629cb6249e0af42744f62afa5deecea8f
Erico Oliveira <ericolvr@gmail.com>
2024-03-25 16:34:23 -0300

    add repositories

--- internal/repository/equipment_repository.go
+++ internal/repository/equipment_repository.go
@@ -9,6 +9,8 @@ import (
 
 type EquipmentRepository interface {
 	Create(equipment *domain.Equipment) error
+	GetEquipments() ([]*domain.Equipment, error)
+	GetEquipmentByID(id int64) (*domain.Equipment, error)
 	GetByIdentifier(identifier string) (*domain.Equipment, error)
 }
 
@@ -58,6 +60,58 @@ func (r *mysqlEquipmentRepository) GetByIdentifier(identifier string) (*domain.E
 
 	var equipment domain.Equipment
 	err := row.Scan(&equipment.ID, &equipment.Identifier)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &equipment, nil
+}
+
+func (r *mysqlEquipmentRepository) GetEquipments() ([]*domain.Equipment, error) {
+	query := "SELECT id, identifier, uniorg, code, used_code, environment, system_name, schedule, users, completed, created_at FROM equipments"
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var equipments []*domain.Equipment
+	for rows.Next() {
+		var equipment domain.Equipment
+		err := rows.Scan(&equipment.ID, &equipment.Identifier, &equipment.Uniorg, &equipment.Code, &equipment.UsedCode, &equipment.Environment, &equipment.SystemName, &equipment.Schedule, &equipment.Users, &equipment.Completed, &equipment.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		equipments = append(equipments, &equipment)
+	}
+
+	return equipments, nil
+}
+
+func (r *mysqlEquipmentRepository) GetEquipmentByID(id int64) (*domain.Equipment, error) {
+	query := "SELECT id, identifier, uniorg, code, used_code, " +
+		"environment, system_name, schedule, users, completed," +
+		"created_at FROM equipments WHERE id = ?"
+	row := r.db.QueryRow(query, id)
+
+	var equipment domain.Equipment
+	err := row.Scan(
+		&equipment.ID,
+		&equipment.Identifier,
+		&equipment.Uniorg,
+		&equipment.Code,
+		&equipment.UsedCode,
+		&equipment.Environment,
+		&equipment.SystemName,
+		&equipment.Schedule,
+		&equipment.Users,
+		&equipment.Completed,
+		&equipment.CreatedAt,
+	)
+
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, nil
