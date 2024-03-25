f925db9ba38b87a2a6f3e3ed7acf225feba0a7e8
Erico Oliveira <ericolvr@gmail.com>
2024-03-25 10:37:19 -0300

    add env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/ericolvr/goapi/internal/adapter/database"
 	"github.com/ericolvr/goapi/internal/adapter/http"
 	"github.com/ericolvr/goapi/internal/repository"
 	"github.com/ericolvr/goapi/internal/usecase"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// initialize router
 	router := gin.Default()
 
+	// load variables
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	databaseName := os.Getenv("DB_NAME")
+	connString := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + databaseName
+
 	// initialize mysql connection
-	db, err := database.NewMySQLConnection("root:secret@tcp(localhost:3306)/goapi")
+	db, err := database.NewMySQLConnection(connString)
 	if err != nil {
 		panic("Failed to connect to MySQL database: " + err.Error())
 	}
