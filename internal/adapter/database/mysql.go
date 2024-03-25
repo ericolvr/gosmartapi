e161aa787459981d89ba53749044b9280804d35e
Erico Oliveira <ericolvr@gmail.com>
2024-03-25 10:52:53 -0300

    added viper

--- internal/adapter/database/mysql.go
+++ internal/adapter/database/mysql.go
@@ -2,12 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
-func NewMySQLConnection(connectionString string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", connectionString)
+func NewMySQLConnection() (*sql.DB, error) {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	// devo adicionar um fallback aqui
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading .env file: %s", err)
+	}
+	user := viper.GetString("DB_USER")
+	password := viper.GetString("DB_PASSWORD")
+	host := viper.GetString("DB_HOST")
+	port := viper.GetString("DB_PORT")
+	databaseName := viper.GetString("DB_NAME")
+	connString := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + databaseName
+
+	db, err := sql.Open("mysql", connString)
 	if err != nil {
 		return nil, err
 	}
