8e5391b3d09142a266c7ab621d52e79575e5625c
Erico Oliveira <ericolvr@gmail.com>
2024-03-26 09:00:15 -0300

    delete

--- internal/adapter/http/equipment_handler.go
+++ internal/adapter/http/equipment_handler.go
@@ -21,6 +21,8 @@ func NewEquipmentHandler(router *gin.Engine, equipmentUsecase usecase.EquipmentU
 	router.POST("/equipments", handler.createEquipment)
 	router.GET("/equipments", handler.getEquipments)
 	router.GET("/equipments/:id", handler.getEquipment)
+	router.PUT("/equipments/:id", handler.updateEquipment)
+	router.DELETE("/users/:id", handler.deleteEquipment)
 }
 
 func (h *equipmentHandler) createEquipment(c *gin.Context) {
@@ -65,4 +67,41 @@ func (h *equipmentHandler) getEquipment(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, equipment)
+}
+
+func (h *equipmentHandler) updateEquipment(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid equipment ID"})
+		return
+	}
+
+	var equipment domain.Equipment
+	if err := c.ShouldBindJSON(&equipment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	equipment.ID = id
+
+	if err := h.equipmentUsecase.UpdateEquipment(&equipment); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, equipment)
+}
+
+func (h *equipmentHandler) deleteEquipment(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid equipment ID"})
+		return
+	}
+
+	if err := h.equipmentUsecase.DeleteEquipment(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Equipment deleted successfully"})
 }
