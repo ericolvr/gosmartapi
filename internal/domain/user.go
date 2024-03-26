090a76b3642c2126ed9c2118344424702be14dd1
Thiago Zilli <thiago.zilli@asapcred.com.br>
2024-03-26 10:20:28 -0300

    feat: user with userResponse

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -9,3 +9,23 @@ type User struct {
 	Photo     string `json:"photo"`
 	Completed bool   `json:"completed"`
 }
+
+type UserResponse struct {
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	Document  string `json:"document"`
+	Role      string `json:"role"`
+	Photo     string `json:"photo"`
+	Completed bool   `json:"completed"`
+}
+
+func (u *User) Response() *UserResponse {
+	return &UserResponse{
+		ID:        u.ID,
+		Name:      u.Name,
+		Document:  u.Document,
+		Role:      u.Role,
+		Photo:     u.Photo,
+		Completed: u.Completed,
+	}
+}
